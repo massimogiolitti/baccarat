329fef1a1d81e4e48a0ad2b90ff8c6c973668220
massimogiolitti <giolittimassimo@gmail.com>
2017-10-13 08:09:45 -0700

    simulator prints to CSV. need to update parser to read from csv instead of txt.

--- baccarat.rb
+++ baccarat.rb
@@ -1,10 +1,9 @@
+require 'csv'
 class Baccarat
 	#need variable shoe cut and variable amount of burn cards
-
 	@shoe_cut = 14
 	puts "shoe cut snoqualmie standards is 14"
 	@cut_card_pulled = false
-	# 1 card then cut card then 5 cards, 6 cards. 3 remain in max case. 
 
 	def self.new_shoe()
 		# deck of 52 cards
@@ -21,11 +20,7 @@ class Baccarat
 		shoe.insert(@shoe_cut,"Cut Card")
 		# First card of the shoe is pulled and a corresponding number of 
 		# cards will be burned.
-		puts shoe.last
 		shoe.last.times {shoe.delete_at(shoe.size - 1)}
-		print shoe
-		puts "shoe size is #{shoe.size}"
-		#abort
 		return shoe
 	end
 
@@ -34,15 +29,12 @@ class Baccarat
 			a[a.size-1] = shoe.pop
 			@cut_card_pulled = true
 		end
-		print a
-		puts
 	end
 	#lots of repetitive if statements. will evenetually fix this. 
 	def self.deal_hand(shoe)
+		hand_outcome = ""
 		p=[shoe.pop]
 		check_cut_card(p, shoe)
-		print p
-		puts 
 		b=[shoe.pop]
 		check_cut_card(b, shoe)
 		p.push(shoe.pop)
@@ -55,20 +47,28 @@ class Baccarat
 
 		if (((p[0] + p[1]) % 10 == 9 && (b[0] + b[1]) % 10 == 9) || ((p[0] + p[1]) % 10 == 8 && (b[0] + b[1]) % 10 == 8))
 			puts "NT"
+			hand_outcome =  "T"
 		elsif ((p[0] + p[1]) % 10 == 9)
 			puts "PN"
+			hand_outcome =  "P"
 		elsif ((b[0] + b[1]) % 10 == 9)
 			puts "BN"
+			hand_outcome =  "B"
 		elsif ((p[0] + p[1]) % 10 == 8)
 			puts "PN"
+			hand_outcome =  "P"
 		elsif ((b[0] + b[1]) % 10 == 8)
 			puts "BN"
+			hand_outcome =  "B"
 		elsif (((p[0] + p[1]) % 10 == 7 && (b[0] + b[1])  % 10 == 7) || ((p[0] + p[1]) % 10 == 6 && (b[0] + b[1]) % 10 == 6))
 			puts "T"
+			hand_outcome =  "T"
 		elsif ((p[0] + p[1]) % 10 == 7 && (b[0] + b[1]) % 10 == 6)
 			puts "PW BBQ"
+			hand_outcome =  "P"
 		elsif ((p[0] + p[1]) % 10 == 6 && (b[0] + b[1]) % 10 == 7)
 			puts "BW BBQ"
+			hand_outcome =  "B"
 		elsif (((p[0] + p[1]) % 10 == 7 || (p[0] + p[1]) % 10 == 6) && (b[0] + b[1]) % 10 < 6)
 			puts "player is 6 or 7 and banker is not"
 			b.push(shoe.pop)
@@ -76,10 +76,13 @@ class Baccarat
 			puts "banker is now #{(b[0] + b[1] + b[2]) % 10}"
 			if ((p[0] + p[1]) % 10 > (b[0] + b[1] + b[2]) % 10)
 				puts "PW"
+				hand_outcome =  "P"
 			elsif ((p[0] + p[1]) % 10 < (b[0] + b[1] + b[2]) % 10)
 				puts "BW"
+				hand_outcome =  "B"
 			else
 				puts "T"
+				hand_outcome =  "T"
 			end
 		elsif (((b[0] + b[1]) % 10 == 7) && (p[0] + p[1]) % 10 < 6)
 			puts "banker is 6 or 7 and player is not"
@@ -88,10 +91,13 @@ class Baccarat
 			puts "player is now #{(p[0] + p[1] + p[2]) % 10}"
 			if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1]) % 10)
 				puts "PW"
+				hand_outcome =  "P"
 			elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1]) % 10)
 				puts "BW"
+				hand_outcome =  "B"
 			else
 				puts "T"
+				hand_outcome =  "T"
 			end
 		elsif ((b[0] + b[1]) % 10 == 6 && (p[0] + p[1]) % 10 < 6)
 			p.push(shoe.pop)
@@ -101,18 +107,24 @@ class Baccarat
 				check_cut_card(b, shoe)
 				if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1] + b[2]) % 10)
 					puts "PW"
+					hand_outcome =  "P"
 				elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1] + b[2]) % 10)
 					puts "BW"
+					hand_outcome =  "B"
 				else
 					puts "T"
+					hand_outcome =  "T"
 				end
 			else 
 				if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1]) % 10)
 					puts "PW"
+					hand_outcome =  "P"
 				elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1]) % 10)
 					puts "BW"
+					hand_outcome =  "B"
 				else
 					puts "T"
+					hand_outcome =  "T"
 				end
 			end
 		elsif ((p[0] + p[1]) % 10 < 6 && (b[0] + b[1]) % 10 < 6)
@@ -125,18 +137,24 @@ class Baccarat
 					check_cut_card(b, shoe)
 					if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1] + b[2]) % 10)
 						puts "PW"
+						hand_outcome =  "P"
 					elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1] + b[2]) % 10)
 						puts "BW"
+						hand_outcome =  "B"
 					else
 						puts "T, Banker at #{(b[0] + b[1] + b[2]) % 10} and player at #{(p[0] + p[1] + p[2]) % 10}"
+						hand_outcome =  "T"
 					end
 				else 
 					if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1]) % 10)
 						puts "PW"
+						hand_outcome =  "P"
 					elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1]) % 10)
 						puts "BW"
+						hand_outcome =  "B"
 					else
 						puts "T, Banker at #{(b[0] + b[1]) % 10} and player at #{(p[0] + p[1] + p[2]) % 10}"
+						hand_outcome =  "T"
 					end
 				end
 
@@ -146,38 +164,50 @@ class Baccarat
 					check_cut_card(b, shoe)
 					if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1] + b[2]) % 10)
 						puts "PW"
+						hand_outcome =  "P"
 					elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1] + b[2]) % 10)
 						puts "BW"
+						hand_outcome =  "B"
 					else
 						puts "T, Banker at #{(b[0] + b[1] + b[2]) % 10} and player at #{(p[0] + p[1] + p[2]) % 10}"
+						hand_outcome =  "T"
 					end
 				else 
 					if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1]) % 10)
 						puts "PW"
+						hand_outcome =  "P"
 					elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1]) % 10)
 						puts "BW"
+						hand_outcome =  "B"
 					else
 						puts "T, Banker at #{(b[0] + b[1]) % 10} and player at #{(p[0] + p[1] + p[2]) % 10}"
+						hand_outcome =  "T"
 					end
 				end
 			elsif ((b[0] + b[1]) % 10 == 3)
 				if (p[2] == 8)
 					if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1]) % 10)
 						puts "PW"
+						hand_outcome =  "P"
 					elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1]) % 10)
 						puts "BW"
+						hand_outcome =  "B"
 					else
 						puts "T, Banker at #{(b[0] + b[1]) % 10} and player at #{(p[0] + p[1] + p[2]) % 10}"
+						hand_outcome =  "T"
 					end
 				else 
 					b.push(shoe.pop)
 					check_cut_card(b, shoe)
 					if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1] + b[2]) % 10)
 						puts "PW"
+						hand_outcome =  "P"
 					elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1] + b[2]) % 10)
 						puts "BW"
+						hand_outcome =  "B"
 					else
 						puts "T, Banker at #{(b[0] + b[1] + b[2]) % 10} and player at #{(p[0] + p[1] + p[2]) % 10}"
+						hand_outcome =  "T"
 					end
 				end
 			else 
@@ -185,27 +215,33 @@ class Baccarat
 				check_cut_card(b, shoe)
 				if ((p[0] + p[1] + p[2]) % 10 > (b[0] + b[1] + b[2]) % 10)
 					puts "PW"
+					hand_outcome =  "P"
 				elsif ((p[0] + p[1] + p[2]) % 10 < (b[0] + b[1] + b[2]) % 10)
 					puts "BW"
+					hand_outcome =  "B"
 				else
 					puts "T, Banker at #{(b[0] + b[1] + b[2]) % 10} and player at #{(p[0] + p[1] + p[2]) % 10}"
+					hand_outcome =  "T"
 				end
 			end
 		end
+		return hand_outcome
 	end
 
 	#if the next 6 cards include the cut card remove cut card, flag, exit loop, run deal_hand once more. 
 
-	def self.play_shoe(num_hands, shoe)
+	def self.play_shoe(shoe)
 		@cut_card_pulled = false
-		while (num_hands > 0 && !@cut_card_pulled)
-			deal_hand(shoe)
-			num_hands -= 1
-			puts num_hands
+		shoe_outcome = []
+		while (!@cut_card_pulled)
+			shoe_outcome << deal_hand(shoe)
 		end
-		deal_hand(shoe)
+		shoe_outcome << deal_hand(shoe)
 		puts "SHOE HAS ENDED"
+		return shoe_outcome
 	end
 
-	100.times {play_shoe(85,new_shoe())}
+	CSV.open("/Users/massimo/desktop/baccarat/bac.csv", "w") do |csv|
+		100.times { |n| csv << play_shoe(new_shoe()).insert(0,n+1)}
+	end
 end
